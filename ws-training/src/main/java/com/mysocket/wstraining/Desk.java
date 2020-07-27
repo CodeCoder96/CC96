96bd5c04f8c5b6bcc66bf3d05f02c48560b2cce7
CodeCoder96 <yfurkan1996@gmail.com>
2020-07-27 13:48:14 +0300

    Web Socket Training

---
+++ ws-training/src/main/java/com/mysocket/wstraining/Desk.java
@@ -0,0 +1,20 @@
+package com.mysocket.wstraining;
+
+public class Desk {
+	
+	private String desk;
+	
+	public Desk() {
+		
+	}
+	
+	public Desk(String desk) {
+		this.desk = desk;
+	}
+
+	public String getDesk() {
+		return desk;
+	}
+	
+	
+}
---
+++ ws-training/src/main/java/com/mysocket/wstraining/WsTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.mysocket.wstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsTrainingApplication.class, args);
+	}
+
+}
